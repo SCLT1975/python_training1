d6c412ef30f2c345ee701369c8073bf13468cfab
mmaksimovich <mmaksimovich@4xxi.com>
2018-05-23 11:16:06 +0300

    созданы тесты по добавлению контактов

--- contact.py
+++ contact.py
@@ -3,7 +3,25 @@
 
 class Contact:
 
-    def __init__(self, name, header, footer):
+    def __init__(self, name, middle_name, last_name, nick_name, title, company, address, t_home, t_mobile, t_work, t_fax, email1, email2, email3, homepage, address2, home2, notes, b_year, a_year):
         self.name = name
-        self.header = header
-        self.footer = footer
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nick_name = nick_name
+        self.title = title
+        self.company = company
+        self.address = address
+        self.t_home = t_home
+        self.t_mobile = t_mobile
+        self.t_work = t_work
+        self.t_fax = t_fax
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.address2 = address2
+        self.home2 = home2
+        self.notes = notes
+        self.b_year = b_year
+        self.a_year = a_year
+
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from group import Group
+from contact import Contact
 import unittest
 
 
@@ -21,44 +21,62 @@ class test_add_group_py(unittest.TestCase):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="qwerty", header="123", footer="wqertyu"))
-        self.return_to_groups(wd)
+        self.open_contacts_page(wd)
+        self.create_contact(wd, Contact(name="qwerty", middle_name="123", last_name="wqertyu", nick_name='wer',
+                                      title='brsf', company='ertw', address='qwer', t_home='dsaf', t_mobile='234', t_work='345',
+                                      t_fax='345', email1='234', email2='534', email3='2134', homepage='5423', address2='3425', home2='gertgertg', notes='btertgwertgretg', b_year=2010, a_year='1987'))
         self.logout(wd)
 
-    def test_test_add_empty_group_py(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups(wd)
-        self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def return_to_groups(self, wd):
-        wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd, group):
-        # start group creation
-        wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
-        # Fill out groups' form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+    def create_contact(self, wd, contact):
+        # Fill out сontacts' textfields
+        self.fill_out_field(wd, 'firstname', contact.name)
+        self.fill_out_field(wd, 'middlename', contact.middle_name)
+        self.fill_out_field(wd, 'lastname', contact.last_name)
+        self.fill_out_field(wd, 'nickname', contact.nick_name)
+        self.fill_out_field(wd, 'title', contact.title)
+        self.fill_out_field(wd, 'company', contact.company)
+        self.fill_out_field(wd, 'address', contact.address)
+        self.fill_out_field(wd, 'home', contact.t_home)
+        self.fill_out_field(wd, 'mobile', contact.t_mobile)
+        self.fill_out_field(wd, 'work', contact.t_work)
+        self.fill_out_field(wd, 'fax', contact.t_fax)
+        self.fill_out_field(wd, 'email', contact.email1)
+        self.fill_out_field(wd, 'email2', contact.email2)
+        self.fill_out_field(wd, 'email3', contact.email3)
+        self.fill_out_field(wd, 'homepage', contact.homepage)
+        self.fill_out_field(wd, 'address2', contact.address2)
+        self.fill_out_field(wd, 'phone2', contact.home2)
+        self.fill_out_field(wd, 'notes', contact.notes)
+        # fill out contacts' datapickers
+        wd.find_element_by_name('bday').click()
+        wd.find_element_by_xpath("//select/option[7]").click()
+        wd.find_element_by_name('bmonth').click()
+        wd.find_element_by_xpath("//select/option[@value='January']").click()
+        wd.find_element_by_name('aday').click()
+        wd.find_element_by_xpath("//select[3]/option[20]").click()
+        wd.find_element_by_name('amonth').click()
+        wd.find_element_by_xpath("//select[4]/option[7]").click()
+        self.fill_out_field(wd, 'byear', contact.b_year)
+        self.fill_out_field(wd, 'ayear', contact.a_year)
+
+
+
         # submit group creation
         wd.find_element_by_name("submit").click()
 
+
+    def fill_out_field(self, wd, elements_name, filled_data):
+        wd.find_element_by_name(elements_name).click()
+        wd.find_element_by_name(elements_name).clear()
+        wd.find_element_by_name(elements_name).send_keys(filled_data)
+
     def open_contacts_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_link_text("add new").click()
 
     def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
